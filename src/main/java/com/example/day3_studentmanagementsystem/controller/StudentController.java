d92b2c1bc2e893cb539c0b0c46f6a2454e8cace4
maheshgoyal-sys <maheshgoyal20032004@gmail.com>
2026-01-20 12:45:10 +0530

    Added StudentModel.java

---
+++ src/main/java/com/example/day3_studentmanagementsystem/controller/StudentController.java
@@ -0,0 +1,16 @@
+package com.example.day3_studentmanagementsystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    @GetMapping("/")
+
+
+
+
+        public String student(){
+        return "student ho me";
+        }
+}
